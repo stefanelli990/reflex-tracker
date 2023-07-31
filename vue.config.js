faae07b2a3c26133ea343d17bc7d24cd731a378a
stefanelli990 <stefan.mohenski@gmail.com>
2023-07-31 20:13:45 +0200

    dsadadsda

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? 'https://github.com/stefanelli990/reflex-tracker.git' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? 'reflex-tracker' : '/',
 })
